74d387147ca0f4509a3c78522ba25e634286ebe1
leo <lkuligin@yandex.ru>
2017-08-03 20:13:22 +0200

    uncomment test2

--- src/test/scala/dynamic/SherlockCostTest.scala
+++ src/test/scala/dynamic/SherlockCostTest.scala
@@ -6,10 +6,10 @@ import org.scalatest.{MustMatchers, WordSpec}
 class SherlockCostTest extends WordSpec with MustMatchers {
   val o = SherlockCost
 
- /* "sherlock cost test1" in {
+  "sherlock cost test1" in {
     val input = "1\n5\n10 1 10 1 10".split("\n").toList
     o.run(input) mustBe List(36)
-  }*/
+  }
 
   "sherlock cost test2" in {
     val input = "1\n5\n100 2 100 2 100".split("\n").toList
