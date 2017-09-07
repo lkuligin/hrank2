cf866844d8bf4f0cc22c80c4cc5d0fe8faec5035
leo <lkuligin@yandex.ru>
2017-09-07 11:21:38 +0200

    misere nim

---
+++ src/main/scala/algo/game/MisereNim.scala
@@ -0,0 +1,26 @@
+package algo.game
+
+object MisereNim {
+  case class Game(piles: Int, stones: List[Int])
+
+  def solve(input: Game): Boolean = {
+    val stonesAmount = input.stones.foldLeft(0)(_ + _)
+    val stonesXor = input.stones.foldLeft(0)(_ ^ _)
+    if (stonesAmount == input.stones.length) (stonesAmount % 2 == 0)
+    else (stonesXor > 0)
+  }
+
+  def main(args: Array[String]) = {
+    val sc = new java.util.Scanner(System.in)
+    val t = sc.nextInt()
+    val testCases = new Array[Game](t)
+    (0 to t-1).foreach(testCases(_) = {
+      val n = sc.nextInt()
+      val l = List.fill(n)(1).foldLeft(List[Int]())((acc, _) => sc.nextInt() :: acc).reverse
+      Game(n, l)
+    })
+    sc.close()
+
+    (0 to t-1).foreach(x => if (solve(testCases(x))) println("First") else println("Second"))
+  }
+}
